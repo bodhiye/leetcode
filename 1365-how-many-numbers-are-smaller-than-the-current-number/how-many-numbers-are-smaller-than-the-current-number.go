166a232393d226eb59b7e88088b5b5b495c06e2c
yeqiongzhou <yeqiongzhou@qiniu.com>
2020-12-21 14:33:42 +0800

    how-many-numbers-are-smaller-than-the-current-number

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{8, 1, 2, 2, 3}
+	fmt.Print(smallerNumbersThanCurrent(nums))
+}
+
+type pair struct {
+	Pos   int
+	Value int
+}
+
+func smallerNumbersThanCurrent(nums []int) []int {
+	p := make([]pair, len(nums))
+	for i, num := range nums {
+		p[i] = pair{i, num}
+	}
+	sort.Slice(p, func(i, j int) bool { return p[i].Value < p[j].Value })
+
+	res := make([]int, len(nums))
+	var small int
+	for i := range p {
+		if i > 0 && p[i].Value != p[i-1].Value {
+			small = i
+		}
+		res[p[i].Pos] = small
+	}
+
+	return res
+}
